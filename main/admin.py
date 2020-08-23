51960eaad9aa5cff98ed671075c5817f91fa6596
Innocent Zenda <zendainnocent@gmail.com>
2020-08-23 11:56:02 +0300

    Display checkout data in admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
-from .models import ProductTag, ProductImage, Product
+from .models import (ProductTag, ProductImage, Product,
+                     Basket, BasketLine, Order, OrderLine)
 
 admin.AdminSite.site_title = 'BookTime'
 admin.AdminSite.site_header = 'BookTime administration'
@@ -40,3 +41,53 @@ class ProductImageAdmin(admin.ModelAdmin):
 
     def product_name(self, obj):
         return obj.product.name
+
+
+class BasketLineInline(admin.TabularInline):
+    model = BasketLine
+    raw_id_fields = ('product', )
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'count', )
+    list_editable = ('status', )
+    list_filter = ('status', )
+    inlines = (BasketLineInline, )
+
+
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+    raw_id_fields = ('product', )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', )
+    list_editable = ('status', )
+    list_filter = ('status', 'shipping_country', 'date_added', )
+    list_display_links = ('user', )
+    inlines = (OrderLineInline, )
+    fieldsets = (
+        (None, {'fields': ('user', 'status', )}),
+        ('Billing Info',{
+            'fields': (
+                'billing_name',
+                'billing_address1',
+                'billing_address2',
+                'billing_zip_code',
+                'billing_city',
+                'billing_country',
+            )
+        }),
+        ('Shipping Info', {
+            'fields': (
+                'shipping_name',
+                'shipping_address1',
+                'shipping_address2',
+                'shipping_zip_code',
+                'shipping_city',
+                'shipping_country',
+            )
+        }),
+    )
